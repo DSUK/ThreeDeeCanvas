97e8f96490b5ce803bb54c376bb0bd14ffd038ef
James <james_gilbert100@yahoo.co.uk>
2017-01-17 10:54:40 +0000

    Disabled turbolinks, for now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
+//  require turbolinks //causes problems with angular
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
